dc0f1cf7c032b6149a76b8f79be0d6e52ab0e484
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2023-08-18 20:21:17 +0200

    Added vueuse module for nuxt-3

---
+++ components/HeaderSearch.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex items-center border p-1 rounded-md">
+    <input
+      placeholder="Search Products"
+      class="px-2"
+      ref="inputRef"
+      type="text"
+    />
+    <SvgoUiSearch class="w-5" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const inputRef = ref();
+const { focused } = useFocus(inputRef);
+
+watch(focused, () => {
+  console.log("Input field focused");
+});
+</script>
