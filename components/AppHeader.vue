771e9634fdc4e9b146acb58b2cb55d556c7b436f
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2023-08-18 21:34:20 +0200

    Move icons

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,14 +2,14 @@
   <header>
     <div class="border-b flex py-4 items-center">
       <nuxt-link to="/">
-        <SvgoLogo class="w-32" :font-controlled="false" />
+        <SvgoUiLogo class="w-32" :font-controlled="false" />
       </nuxt-link>
-      <div class="flex-1 flex justify-end space-x-2">
+      <div class="flex-1 flex justify-end space-x-3 items-center">
         <HeaderSearch />
         <NuxtLink to="women">Women</NuxtLink>
         <NuxtLink to="men">Men</NuxtLink>
         <NuxtLink to="sale">Sale</NuxtLink>
-        <SvgoUser class="w-4" />
+        <SvgoUiUser class="w-4" />
         <SvgoUiBasket class="w-5 h-5" />
       </div>
     </div>
