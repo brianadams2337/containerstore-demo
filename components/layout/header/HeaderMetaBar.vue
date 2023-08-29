8c6fe39e7faee960de6ce8c9ecca142379eae2b4
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2023-08-29 12:23:15 +0200

    Convert from px to rem

--- components/layout/header/HeaderMetaBar.vue
+++ components/layout/header/HeaderMetaBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-[30px] w-full bg-primary">
+  <div class="h-[1.875rem] w-full bg-primary">
     <div class="flex h-full justify-end">
       <ShopSwitcher class="mr-2 bg-primary text-white" />
     </div>
