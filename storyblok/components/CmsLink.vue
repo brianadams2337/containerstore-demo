1c96f941b7a9c72567fc67be5ffd989b9b6683c5
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2023-09-05 13:21:34 +0200

    Added CmsLink component

---
+++ storyblok/components/CmsLink.vue
@@ -0,0 +1,20 @@
+<template>
+  <RawLink
+    v-if="url"
+    :to="url"
+    :target="blok.open_in_new_tab ? '_blank' : '_self'">
+    {{ blok.label }}
+  </RawLink>
+</template>
+<script lang="ts" setup>
+import { LinkStoryblok } from '../types/component-types-sb'
+
+const props = defineProps({
+  blok: {
+    type: Object as PropType<LinkStoryblok>,
+    required: true,
+  },
+})
+
+const url = props.blok?.cta_url?.cached_url ?? null
+</script>
