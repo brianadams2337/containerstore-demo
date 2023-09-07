e1bd75e8e303f93fd294c1fcba6135ef9ba80ad7
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2023-09-07 13:58:13 +0200

    Update env example & environment file

--- environment.ts
+++ environment.ts
@@ -5,36 +5,27 @@ export default cleanEnv(
   process.env,
   {
     BASE_URL: url(),
+
     REDIS_HOST: host({ default: 'localhost' }),
     REDIS_PORT: num({ default: 6379 }),
     REDIS_PREFIX: str({ default: '' }),
-    REDIS_SSL_TRANSIT: bool({ default: false }),
 
     BAPI_HOST: url(),
     BAPI_TOKEN: str(),
 
     AY_CACHE_DISABLED: bool({ default: false }),
-    STORYBLOK_ACCESS_TOKEN: str(),
-    DEBUG_LEVEL: str({
-      default: '',
-      choices: ['fatal', 'error', 'warn', 'info', 'debug', ''],
-    }),
 
     DOMAIN_EN: str({ default: '' }),
     DOMAIN_DE_DE: str({ default: '' }),
     DOMAIN_DE_AT: str({ default: '' }),
     DOMAIN_DE_CH: str({ default: '' }),
     DOMAIN_DEFAULT: str({ default: '' }),
-    DOMAIN_PER_LOCALE: bool({ default: true }),
-
-    CHECKOUT_ACCESS_HEADER: str({ default: undefined }),
 
     CHECKOUT_HOST: str({ default: '' }),
     CHECKOUT_TOKEN_1001: str(),
     CHECKOUT_SECRET_1001: str(),
     CHECKOUT_USER_1001: str(),
-
-    GOOGLE_TAG_MANAGER_ID: str({ default: '' }),
+    CHECKOUT_ACCESS_HEADER: str({ default: undefined }),
 
     BASIC_AUTH: str({ default: '' }),
 
@@ -43,12 +34,10 @@ export default cleanEnv(
     HTTPS_KEY: str({ default: '' }),
     HTTPS_CERT: str({ default: '' }),
 
-    ACTIVE_LPL_1001: bool({ default: false }),
+    STORYBLOK_ACCESS_TOKEN: str(),
     STORYBLOK_WEBHOOK_SECRET: str({
       default: 'hereupon-caviar-nicety-wanton',
     }),
-
-    SENTRY_DSN: str({ default: '' }),
   },
   {
     reporter: (options) => {
