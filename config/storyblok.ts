3d368b4087f8c8abcc71a58270059eeed14d7683
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2023-09-15 09:35:13 +0200

    refactor(nuxt3): remove duplicate storyblok.accessToken and rely on runtimeConfig

--- config/storyblok.ts
+++ config/storyblok.ts
@@ -1,6 +1,4 @@
 import environment from '../environment'
 
 // https://github.com/storyblok/storyblok-nuxt#options
-export default {
-  accessToken: environment.STORYBLOK_ACCESS_TOKEN,
-}
+export default {}
