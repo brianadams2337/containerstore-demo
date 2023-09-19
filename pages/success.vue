4cf44e455e3811d78476754057a8b86a2335dac0
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2023-09-19 07:59:03 +0200

    Add optional operator on order items

--- pages/success.vue
+++ pages/success.vue
@@ -108,7 +108,7 @@ const getItemQuantity = (variantId: number): number | undefined => {
 }
 
 const orderItems = computed(() => {
-  return useUnique(orderData.value.items || [], (it: any) => it.variant.id)
+  return useUnique(orderData.value?.items || [], (it: any) => it.variant.id)
 })
 </script>
 
