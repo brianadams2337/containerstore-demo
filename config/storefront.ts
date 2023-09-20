brianadams2337/acme-shop-flo

build(nuxt3): adapt runtimeConfig override env variables as default

config/storefront.ts
@@ -4,7 +4,7 @@ import * as customRpcMethods from '../rpcMethods'
 import withParams from '../constants/withParams'
 
 const baseShopConfig = {
-  domain: environment.DOMAIN_DEFAULT,
+  domain: environment.NUXT_STOREFRONT_DOMAIN_DEFAULT,
   paymentProviders: [
     'lastschrift',
     'visa',
@@ -15,7 +15,6 @@ const baseShopConfig = {
     'klarna',
     'paypal',
   ],
-  storeCampaignKeyword: environment.CAMPAIGN_KEY_PREFIX,
   appKeys: {
     wishlistKey: 'wishlist_{shopId}_{userId}',
     basketKey: 'basket_{shopId}_{userId}',
@@ -54,6 +53,18 @@ const shops = [
     shopId: 1001,
     currency: 'EUR',
   },
+  {
+    locale: 'de-AT',
+    path: 'de',
+    shopId: 1001,
+    currency: 'EUR',
+  },
+  {
+    locale: 'de-CH',
+    path: 'de',
+    shopId: 1001,
+    currency: 'EUR',
+  },
 ]
 
 const protocol =
@@ -63,65 +74,78 @@ const protocol =
     : 'http://'
 
 export const storefrontRuntimeConfigPrivate: Partial<ModuleOptions> = {
-  // Following keys are overridable using prefix NUXT_$STOREFRONT_REDIRECTS
+  // Following keys are overridable using prefix NUXT_PUBLIC_PUBLIC_SHOP_DATA
+  publicShopData: ['paymentProviders', 'isLowestPreviousPriceActive'],
+  // Following keys are overridable using prefix NUXT_STOREFRONT_REDIRECTS
   redirects: {
-    enabled: Boolean(process.env.NUXT_$STOREFRONT_REDIRECTS_ENABLED),
+    enabled: Boolean(process.env.NUXT_STOREFRONT_REDIRECTS_ENABLED),
     queryParamWhitelist: [],
   },
-  // Following keys are overridable using prefix NUXT_$STOREFRONT_SESSION
+  // Following keys are overridable using prefix NUXT_STOREFRONT_SESSION
   session: {
     sameSite: process.env.APP_ENV !== 'production' ? 'none' : 'lax',
     maxAge: 2419200000, // four weeks in milliseconds
     provider: 'redis',
   },
-  // Following keys are overridable using prefix NUXT_$STOREFRONT_BAPI
+  // Following keys are overridable using prefix NUXT_STOREFRONT_BAPI
   bapi: {
-    host: environment.BAPI_HOST,
-    token: environment.BAPI_TOKEN,
+    host: environment.NUXT_STOREFRONT_BAPI_HOST,
+    token: environment.NUXT_STOREFRONT_BAPI_TOKEN,
     // TODO: Is shopId required here for tenants that need to separate shop/customer data?
   },
-  // Following keys are overridable using prefix NUXT_$STOREFRONT_OAUTH
+  // Following keys are overridable using prefix NUXT_STOREFRONT_OAUTH
   oauth: {
-    host: environment.OAUTH_API_HOST,
-    clientId: environment.OAUTH_CLIENT_ID,
-    clientSecret: environment.OAUTH_CLIENT_SECRET,
+    host: environment.NUXT_STOREFRONT_OAUTH_API_HOST,
+    clientId: environment.NUXT_STOREFRONT_OAUTH_CLIENT_ID,
+    clientSecret: environment.NUXT_STOREFRONT_OAUTH_CLIENT_SECRET,
   },
-  // Following keys are overridable using prefix NUXT_STOREFRONT_PUBLIC_SHOP_DATA
-  publicShopData: ['paymentProviders', 'isLowestPreviousPriceActive'],
-  // Following keys are overridable using prefix NUXT_$STOREFRONT_SHOP_SELECTOR
-  shopSelector: environment.DOMAIN_PER_LOCALE ? 'domain' : 'path',
-  // Following keys are overridable using prefix NUXT_$STOREFRONT_STORES
-  stores: shops.map((shop) => ({
-    ...baseShopConfig,
-    shopId: shop.shopId,
-    path: shop.path,
-    locale: shop.locale,
-    isLowestPreviousPriceActive:
-      environment.IS_LOWEST_PREVIOUS_PRICE_ACTIVE_1001,
-    auth: {
-      resetPasswordUrl: `${protocol}${shop.locale}/signin/`,
-    },
-    currency: shop.currency,
-    checkout: {
-      shopId: shop.shopId,
-      // Checkout probably isn't configured for the non-DE shops
-      // but the main goal is testing storefront
-      token: environment.CHECKOUT_TOKEN_1001,
-      secret: environment.CHECKOUT_SECRET_1001,
-      host: environment.CHECKOUT_HOST,
-      user: environment.CHECKOUT_USER_1001,
-    },
-  })),
-  // Following keys are overridable using prefix NUXT_$STOREFRONT_REDIS
+  // Following keys are overridable using prefix NUXT_STOREFRONT_SHOP_SELECTOR
+  shopSelector: environment.NUXT_STOREFRONT_DOMAIN_PER_LOCALE ? 'domain' : 'path',
+  // Following keys are overridable using prefix NUXT_STOREFRONT_STORES
+  stores: shops.reduce(
+    (previousShopConfigs, shop) => ({
+      // Values within `storefront.stores` are overridable by using their locale as identifier.
+      // Example of an runtimeConfig override: NUXT_STOREFRONT_STORES_EN_US_PATH=someValue
+      // We use environment.NUXT_STOREFRONT_STORES_{LOCALE}_SOME_VALUE only to set some empty default,
+      // as all values should be provided through runtime using NUXT_ environment variable overrides.
+      // https://nuxt.com/docs/guide/going-further/runtime-config#example
+      ...previousShopConfigs,
+      // Use shop.locale instead of shop.shopId to avoid conflicts if we use the same id
+      [shop.locale]: {
+        ...baseShopConfig,
+        shopId: shop.shopId,
+        path: shop.path,
+        locale: shop.locale,
+        isLowestPreviousPriceActive:
+          environment.NUXT_STOREFRONT_STORES_EN_US_IS_LOWEST_PREVIOUS_PRICE_ACTIVE,
+        auth: {
+          resetPasswordUrl: `${protocol}${shop.locale}/signin/`,
+        },
+        storeCampaignKeyword: environment.NUXT_STOREFRONT_STORES_EN_US_CAMPAIGN_KEYWORD,
+        currency: shop.currency,
+        checkout: {
+          shopId: shop.shopId,
+          // Checkout probably isn't configured for the non-DE shops
+          // but the main goal is testing storefront
+          token: environment.NUXT_STOREFRONT_STORES_EN_US_CHECKOUT_TOKEN,
+          secret: environment.NUXT_STOREFRONT_STORES_EN_US_CHECKOUT_SECRET,
+          host: environment.NUXT_STOREFRONT_STORES_EN_US_CHECKOUT_HOST,
+          user: environment.NUXT_STOREFRONT_STORES_EN_US_CHECKOUT_USER,
+        }
+      }}
+      ),
+    {}
+  ),
+  // Following keys are overridable using prefix NUXT_STOREFRONT_REDIS
   redis: {
-    host: environment.REDIS_HOST,
-    port: environment.REDIS_PORT,
-    prefix: environment.REDIS_PREFIX,
-    user: process.env.REDIS_USER,
-    password: process.env.REDIS_PASSWORD,
-    sslTransit: Boolean(process.env.SSL_TRANSIT)
+    host: environment.NUXT_STOREFRONT_REDIS_HOST,
+    port: environment.NUXT_STOREFRONT_REDIS_PORT,
+    prefix: environment.NUXT_STOREFRONT_REDIS_PREFIX,
+    user: process.env.NUXT_STOREFRONT_REDIS_USER,
+    password: process.env.NUXT_STOREFRONT_REDIS_PASSWORD,
+    sslTransit: Boolean(process.env.NUXT_STOREFRONT_SSL_TRANSIT)
   },
-  // Following keys are overridable using prefix NUXT_$STOREFRONT_CACHE
+  // Following keys are overridable using prefix NUXT_STOREFRONT_CACHE
   cache: {
     auth: {
       username: 'max',
@@ -140,11 +164,12 @@ export const storefrontRuntimeConfigPrivate: Partial<ModuleOptions> = {
 export const storefrontRuntimeConfigPublic: Partial<ModuleOptions> = {
   // Following keys are overridable using prefix NUXT_PUBLIC_WITH_PARAMS
   withParams,
+  // Following keys are overridable using prefix NUXT_PUBLIC_LOG
   log: {
     name: 'storefront-boilerplate-nuxt',
   },
   // Following keys are overridable using prefix NUXT_PUBLIC_IMAGE_BASE_URL
-  imageBaseUrl: 'https://brb-demo.cdn.aboutyou.cloud/',
+  imageBaseUrl: environment.NUXT_PUBLIC_IMAGE_BASE_URL,
 }
 
 

environment.ts
@@ -4,50 +4,65 @@ import yn from 'yn'
 export default cleanEnv(
   process.env,
   {
+    /** Global Configuration */
     BASE_URL: url(),
-
-    REDIS_HOST: host({ default: 'localhost' }),
-    REDIS_PORT: num({ default: 6379 }),
-    REDIS_PREFIX: str({ default: '' }),
-
-    BAPI_HOST: url(),
-    BAPI_TOKEN: str(),
+    NUXT_PUBLIC_IMAGE_BASE_URL: url({ default: 'https://brb-demo.cdn.aboutyou.cloud/' }),
 
     AY_CACHE_DISABLED: bool({ default: false }),
 
-    DOMAIN_EN: str({ default: '' }),
-    DOMAIN_DE_DE: str({ default: '' }),
-    DOMAIN_DE_AT: str({ default: '' }),
-    DOMAIN_DE_CH: str({ default: '' }),
-    DOMAIN_DEFAULT: str({ default: '' }),
-    DOMAIN_PER_LOCALE: bool({ default: true }),
+    NUXT_CHECKOUT_ACCESS_HEADER: str({ default: undefined }),
 
-    CHECKOUT_HOST: str({ default: '' }),
-    CHECKOUT_TOKEN_1001: str(),
-    CHECKOUT_SECRET_1001: str(),
-    CHECKOUT_USER_1001: str(),
-    CHECKOUT_ACCESS_HEADER: str({ default: undefined }),
-    IS_LOWEST_PREVIOUS_PRICE_ACTIVE_1001: bool({ default: false }),
+    NUXT_STOREFRONT_REDIS_HOST: host({ default: 'localhost' }),
+    NUXT_STOREFRONT_REDIS_PORT: num({ default: 6379 }),
+    NUXT_STOREFRONT_REDIS_PREFIX: str({ default: '' }),
 
-    OAUTH_API_HOST: str({ default: '' }),
-    OAUTH_CLIENT_ID: str({ default: '' }),
-    OAUTH_CLIENT_SECRET: str({ default: '' }),
+    NUXT_STOREFRONT_REDIRECTS_ENABLED: bool({ default: false }),
 
-    BASIC_AUTH: str({ default: '' }),
+    NUXT_STOREFRONT_BAPI_HOST: url(),
+    NUXT_STOREFRONT_BAPI_TOKEN: str(),
 
-    CAMPAIGN_KEY_PREFIX: str(),
+    NUXT_STOREFRONT_OAUTH_API_HOST: str({ default: '' }),
+    NUXT_STOREFRONT_OAUTH_CLIENT_ID: str({ default: '' }),
+    NUXT_STOREFRONT_OAUTH_CLIENT_SECRET: str({ default: '' }),
 
-    HTTPS_KEY: str({ default: '' }),
-    HTTPS_CERT: str({ default: '' }),
+    NUXT_STOREFRONT_DOMAIN_EN: str({ default: '' }),
+    NUXT_STOREFRONT_DOMAIN_DE_DE: str({ default: '' }),
+    NUXT_STOREFRONT_DOMAIN_DE_AT: str({ default: '' }),
+    NUXT_STOREFRONT_DOMAIN_DE_CH: str({ default: '' }),
+    NUXT_STOREFRONT_DOMAIN_DEFAULT: str({ default: '' }),
+    NUXT_STOREFRONT_DOMAIN_PER_LOCALE: bool({ default: true }),
 
-    GOOGLE_TAG_MANAGER_ID: str({ default: '' }),
+    NUXT_PUBLIC_GTM_ID: str({ default: '' }),
 
-    STORYBLOK_ACCESS_TOKEN: str(),
-    STORYBLOK_WEBHOOK_SECRET: str({
+    /* Store-specific Configuration */
+    // NOTE: Replace {YOUR_SHOP_ID} with your actual shopId or if you need to support
+    // multiple shop with the same shopId, use your locale value (e.g. EN_US or DE_DE).
+    // We only need a set of default value here to be prefilled during buildtime,
+    // actual configuration will haben through environment variables.
+    // See .env for potential environment variables setup
+
+    // DE_DE / 1001
+    NUXT_STOREFRONT_STORES_EN_US_CHECKOUT_HOST: str({ default: '' }),
+    NUXT_STOREFRONT_STORES_EN_US_CHECKOUT_TOKEN: str(),
+    NUXT_STOREFRONT_STORES_EN_US_CHECKOUT_SECRET: str(),
+    NUXT_STOREFRONT_STORES_EN_US_CHECKOUT_USER: str(),
+
+    NUXT_STOREFRONT_STORES_EN_US_CAMPAIGN_KEYWORD: str(),
+
+    NUXT_STOREFRONT_STORES_EN_US_IS_LOWEST_PREVIOUS_PRICE_ACTIVE: bool({ default: false }),
+
+    /* Storyblok-specific Configuration */
+    NUXT_STORYBLOK_ACCESS_TOKEN: str(),
+    NUXT_STORYBLOK_WEBHOOK_SECRET: str({
       default: 'hereupon-caviar-nicety-wanton',
     }),
 
-    SENTRY_DSN: str({ default: '' }),
+    /* Sentry-specific Configuration */
+    NUXT_SENTRY_DSN: str({ default: '' }),
+
+    /* Local Development */
+    HTTPS_KEY: str({ default: '' }),
+    HTTPS_CERT: str({ default: '' }),
   },
   {
     reporter: (options) => {

brianadams2337/containerstore-demo

config/storefront.ts
@@ -4,7 +4,7 @@ import * as customRpcMethods from '../rpcMethods'
 import withParams from '../constants/withParams'
 
 const baseShopConfig = {
-  domain: environment.DOMAIN_DEFAULT,
+  domain: environment.NUXT_STOREFRONT_DOMAIN_DEFAULT,
   paymentProviders: [
     'lastschrift',
     'visa',
@@ -15,7 +15,6 @@ const baseShopConfig = {
     'klarna',
     'paypal',
   ],
-  storeCampaignKeyword: environment.CAMPAIGN_KEY_PREFIX,
   appKeys: {
     wishlistKey: 'wishlist_{shopId}_{userId}',
     basketKey: 'basket_{shopId}_{userId}',
@@ -54,6 +53,18 @@ const shops = [
     shopId: 1001,
     currency: 'EUR',
   },
+  {
+    locale: 'de-AT',
+    path: 'at',
+    shopId: 1001,
+    currency: 'EUR',
+  },
+  {
+    locale: 'de-CH',
+    path: 'ch',
+    shopId: 1001,
+    currency: 'EUR',
+  },
 ]
 
 const protocol =
@@ -63,65 +74,78 @@ const protocol =
     : 'http://'
 
 export const storefrontRuntimeConfigPrivate: Partial<ModuleOptions> = {
-  // Following keys are overridable using prefix NUXT_$STOREFRONT_REDIRECTS
+  // Following keys are overridable using prefix NUXT_PUBLIC_PUBLIC_SHOP_DATA
+  publicShopData: ['paymentProviders', 'isLowestPreviousPriceActive'],
+  // Following keys are overridable using prefix NUXT_STOREFRONT_REDIRECTS
   redirects: {
-    enabled: Boolean(process.env.NUXT_$STOREFRONT_REDIRECTS_ENABLED),
+    enabled: Boolean(process.env.NUXT_STOREFRONT_REDIRECTS_ENABLED),
     queryParamWhitelist: [],
   },
-  // Following keys are overridable using prefix NUXT_$STOREFRONT_SESSION
+  // Following keys are overridable using prefix NUXT_STOREFRONT_SESSION
   session: {
     sameSite: process.env.APP_ENV !== 'production' ? 'none' : 'lax',
     maxAge: 2419200000, // four weeks in milliseconds
     provider: 'redis',
   },
-  // Following keys are overridable using prefix NUXT_$STOREFRONT_BAPI
+  // Following keys are overridable using prefix NUXT_STOREFRONT_BAPI
   bapi: {
-    host: environment.BAPI_HOST,
-    token: environment.BAPI_TOKEN,
+    host: environment.NUXT_STOREFRONT_BAPI_HOST,
+    token: environment.NUXT_STOREFRONT_BAPI_TOKEN,
     // TODO: Is shopId required here for tenants that need to separate shop/customer data?
   },
-  // Following keys are overridable using prefix NUXT_$STOREFRONT_OAUTH
+  // Following keys are overridable using prefix NUXT_STOREFRONT_OAUTH
   oauth: {
-    host: environment.OAUTH_API_HOST,
-    clientId: environment.OAUTH_CLIENT_ID,
-    clientSecret: environment.OAUTH_CLIENT_SECRET,
+    host: environment.NUXT_STOREFRONT_OAUTH_API_HOST,
+    clientId: environment.NUXT_STOREFRONT_OAUTH_CLIENT_ID,
+    clientSecret: environment.NUXT_STOREFRONT_OAUTH_CLIENT_SECRET,
   },
-  // Following keys are overridable using prefix NUXT_STOREFRONT_PUBLIC_SHOP_DATA
-  publicShopData: ['paymentProviders', 'isLowestPreviousPriceActive'],
-  // Following keys are overridable using prefix NUXT_$STOREFRONT_SHOP_SELECTOR
-  shopSelector: environment.DOMAIN_PER_LOCALE ? 'domain' : 'path',
-  // Following keys are overridable using prefix NUXT_$STOREFRONT_STORES
-  stores: shops.map((shop) => ({
-    ...baseShopConfig,
-    shopId: shop.shopId,
-    path: shop.path,
-    locale: shop.locale,
-    isLowestPreviousPriceActive:
-      environment.IS_LOWEST_PREVIOUS_PRICE_ACTIVE_1001,
-    auth: {
-      resetPasswordUrl: `${protocol}${shop.locale}/signin/`,
-    },
-    currency: shop.currency,
-    checkout: {
-      shopId: shop.shopId,
-      // Checkout probably isn't configured for the non-DE shops
-      // but the main goal is testing storefront
-      token: environment.CHECKOUT_TOKEN_1001,
-      secret: environment.CHECKOUT_SECRET_1001,
-      host: environment.CHECKOUT_HOST,
-      user: environment.CHECKOUT_USER_1001,
-    },
-  })),
-  // Following keys are overridable using prefix NUXT_$STOREFRONT_REDIS
+  // Following keys are overridable using prefix NUXT_STOREFRONT_SHOP_SELECTOR
+  shopSelector: environment.NUXT_STOREFRONT_DOMAIN_PER_LOCALE ? 'domain' : 'path',
+  // Following keys are overridable using prefix NUXT_STOREFRONT_STORES
+  stores: shops.reduce(
+    (previousShopConfigs, shop) => ({
+      // Values within `storefront.stores` are overridable by using their locale as identifier.
+      // Example of an runtimeConfig override: NUXT_STOREFRONT_STORES_EN_US_PATH=someValue
+      // We use environment.NUXT_STOREFRONT_STORES_{LOCALE}_SOME_VALUE only to set some empty default,
+      // as all values should be provided through runtime using NUXT_ environment variable overrides.
+      // https://nuxt.com/docs/guide/going-further/runtime-config#example
+      ...previousShopConfigs,
+      // Use shop.locale instead of shop.shopId to avoid conflicts if we use the same id
+      [shop.locale]: {
+        ...baseShopConfig,
+        shopId: shop.shopId,
+        path: shop.path,
+        locale: shop.locale,
+        isLowestPreviousPriceActive:
+          environment.NUXT_STOREFRONT_STORES_EN_US_IS_LOWEST_PREVIOUS_PRICE_ACTIVE,
+        auth: {
+          resetPasswordUrl: `${protocol}${shop.locale}/signin/`,
+        },
+        storeCampaignKeyword: environment.NUXT_STOREFRONT_STORES_EN_US_CAMPAIGN_KEYWORD,
+        currency: shop.currency,
+        checkout: {
+          shopId: shop.shopId,
+          // Checkout probably isn't configured for the non-DE shops
+          // but the main goal is testing storefront
+          token: environment.NUXT_STOREFRONT_STORES_EN_US_CHECKOUT_TOKEN,
+          secret: environment.NUXT_STOREFRONT_STORES_EN_US_CHECKOUT_SECRET,
+          host: environment.NUXT_STOREFRONT_STORES_EN_US_CHECKOUT_HOST,
+          user: environment.NUXT_STOREFRONT_STORES_EN_US_CHECKOUT_USER,
+        }
+      }}
+      ),
+    {}
+  ),
+  // Following keys are overridable using prefix NUXT_STOREFRONT_REDIS
   redis: {
-    host: environment.REDIS_HOST,
-    port: environment.REDIS_PORT,
-    prefix: environment.REDIS_PREFIX,
-    user: process.env.REDIS_USER,
-    password: process.env.REDIS_PASSWORD,
-    sslTransit: Boolean(process.env.SSL_TRANSIT)
+    host: environment.NUXT_STOREFRONT_REDIS_HOST,
+    port: environment.NUXT_STOREFRONT_REDIS_PORT,
+    prefix: environment.NUXT_STOREFRONT_REDIS_PREFIX,
+    user: process.env.NUXT_STOREFRONT_REDIS_USER,
+    password: process.env.NUXT_STOREFRONT_REDIS_PASSWORD,
+    sslTransit: Boolean(process.env.NUXT_STOREFRONT_SSL_TRANSIT)
   },
-  // Following keys are overridable using prefix NUXT_$STOREFRONT_CACHE
+  // Following keys are overridable using prefix NUXT_STOREFRONT_CACHE
   cache: {
     auth: {
       username: 'max',
@@ -140,11 +164,12 @@ export const storefrontRuntimeConfigPrivate: Partial<ModuleOptions> = {
 export const storefrontRuntimeConfigPublic: Partial<ModuleOptions> = {
   // Following keys are overridable using prefix NUXT_PUBLIC_WITH_PARAMS
   withParams,
+  // Following keys are overridable using prefix NUXT_PUBLIC_LOG
   log: {
     name: 'storefront-boilerplate-nuxt',
   },
   // Following keys are overridable using prefix NUXT_PUBLIC_IMAGE_BASE_URL
-  imageBaseUrl: 'https://brb-demo.cdn.aboutyou.cloud/',
+  imageBaseUrl: environment.NUXT_PUBLIC_IMAGE_BASE_URL,
 }
 
 

environment.ts
@@ -4,50 +4,63 @@ import yn from 'yn'
 export default cleanEnv(
   process.env,
   {
+    /** Global Configuration */
     BASE_URL: url(),
-
-    REDIS_HOST: host({ default: 'localhost' }),
-    REDIS_PORT: num({ default: 6379 }),
-    REDIS_PREFIX: str({ default: '' }),
-
-    BAPI_HOST: url(),
-    BAPI_TOKEN: str(),
+    NUXT_PUBLIC_IMAGE_BASE_URL: url({ default: 'https://brb-demo.cdn.aboutyou.cloud/' }),
 
     AY_CACHE_DISABLED: bool({ default: false }),
 
-    DOMAIN_EN: str({ default: '' }),
-    DOMAIN_DE_DE: str({ default: '' }),
-    DOMAIN_DE_AT: str({ default: '' }),
-    DOMAIN_DE_CH: str({ default: '' }),
-    DOMAIN_DEFAULT: str({ default: '' }),
-    DOMAIN_PER_LOCALE: bool({ default: true }),
+    NUXT_CHECKOUT_ACCESS_HEADER: str({ default: undefined }),
 
-    CHECKOUT_HOST: str({ default: '' }),
-    CHECKOUT_TOKEN_1001: str(),
-    CHECKOUT_SECRET_1001: str(),
-    CHECKOUT_USER_1001: str(),
-    CHECKOUT_ACCESS_HEADER: str({ default: undefined }),
-    IS_LOWEST_PREVIOUS_PRICE_ACTIVE_1001: bool({ default: false }),
+    NUXT_STOREFRONT_REDIS_HOST: host({ default: 'localhost' }),
+    NUXT_STOREFRONT_REDIS_PORT: num({ default: 6379 }),
+    NUXT_STOREFRONT_REDIS_PREFIX: str({ default: '' }),
 
-    OAUTH_API_HOST: str({ default: '' }),
-    OAUTH_CLIENT_ID: str({ default: '' }),
-    OAUTH_CLIENT_SECRET: str({ default: '' }),
+    NUXT_STOREFRONT_REDIRECTS_ENABLED: bool({ default: false }),
 
-    BASIC_AUTH: str({ default: '' }),
+    NUXT_STOREFRONT_BAPI_HOST: url(),
+    NUXT_STOREFRONT_BAPI_TOKEN: str(),
 
-    CAMPAIGN_KEY_PREFIX: str(),
+    NUXT_STOREFRONT_OAUTH_API_HOST: str({ default: '' }),
+    NUXT_STOREFRONT_OAUTH_CLIENT_ID: str({ default: '' }),
+    NUXT_STOREFRONT_OAUTH_CLIENT_SECRET: str({ default: '' }),
 
-    HTTPS_KEY: str({ default: '' }),
-    HTTPS_CERT: str({ default: '' }),
+    NUXT_STOREFRONT_DOMAIN_DEFAULT: str({ default: '' }),
+    NUXT_STOREFRONT_DOMAIN_PER_LOCALE: bool({ default: true }),
 
-    GOOGLE_TAG_MANAGER_ID: str({ default: '' }),
+    NUXT_PUBLIC_GTM_ID: str({ default: '' }),
 
-    STORYBLOK_ACCESS_TOKEN: str(),
-    STORYBLOK_WEBHOOK_SECRET: str({
+    /* Store-specific Configuration */
+    // NOTE: Replace {YOUR_SHOP_ID} with your actual shopId or if you need to support
+    // multiple shop with the same shopId, use your locale value (e.g. EN_US or DE_DE).
+    // We only need a set of default value here to be prefilled during buildtime,
+    // actual configuration will haben through environment variables.
+    // See .env for potential environment variables setup
+
+    // DE_DE / 1001
+    NUXT_STOREFRONT_STORES_EN_US_CHECKOUT_HOST: str({ default: '' }),
+    NUXT_STOREFRONT_STORES_EN_US_CHECKOUT_TOKEN: str(),
+    NUXT_STOREFRONT_STORES_EN_US_CHECKOUT_SECRET: str(),
+    NUXT_STOREFRONT_STORES_EN_US_CHECKOUT_USER: str(),
+
+    NUXT_STOREFRONT_STORES_EN_US_DOMAIN: str({ default: '' }),
+
+    NUXT_STOREFRONT_STORES_EN_US_CAMPAIGN_KEYWORD: str(),
+
+    NUXT_STOREFRONT_STORES_EN_US_IS_LOWEST_PREVIOUS_PRICE_ACTIVE: bool({ default: false }),
+
+    /* Storyblok-specific Configuration */
+    NUXT_STORYBLOK_ACCESS_TOKEN: str(),
+    NUXT_STORYBLOK_WEBHOOK_SECRET: str({
       default: 'hereupon-caviar-nicety-wanton',
     }),
 
-    SENTRY_DSN: str({ default: '' }),
+    /* Sentry-specific Configuration */
+    NUXT_SENTRY_DSN: str({ default: '' }),
+
+    /* Local Development */
+    HTTPS_KEY: str({ default: '' }),
+    HTTPS_CERT: str({ default: '' }),
   },
   {
     reporter: (options) => {