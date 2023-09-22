73203791117fff5304909d245cfbf0c017e3d534
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2023-09-22 17:11:53 +0200

    Added content page with cms integration

---
+++ components/ui/Breadcrumbs.vue
@@ -0,0 +1,30 @@
+<template>
+  <HorizontalItemsDivider tag="NuxtLink" v-bind="{ items }" class="mt-1">
+    <template #default="{ items: breadcrumbItems }">
+      <DefaultLink
+        v-for="(link, idx) in asLinks(breadcrumbItems)"
+        :key="link.value"
+        only-exact-active
+        :to="link.to"
+        >{{ link.value }}
+        <template v-if="showDividerTag(idx, breadcrumbItems.length)"
+          >/</template
+        ></DefaultLink
+      >
+    </template>
+  </HorizontalItemsDivider>
+</template>
+
+<script setup lang="ts">
+import { RouteLocationRaw } from '#vue-router'
+type Link = { value: string; to: RouteLocationRaw }
+
+defineProps({
+  items: {
+    type: Array as PropType<Link[]>,
+    required: true,
+  },
+})
+
+const asLinks = (items: any) => items as Link[]
+</script>
---
+++ pages/c/[slug].vue
@@ -0,0 +1,45 @@
+<template>
+  <div v-if="fetching" class="container mx-auto space-y-2 py-4">
+    <div class="w-full max-w-xl">
+      <SkeletonLoader class="mb-10 w-full" full-width />
+    </div>
+    <SkeletonLoader type="custom" class="h-6 w-72" />
+    <SkeletonLoader type="custom" class="h-6 w-48" />
+
+    <SkeletonLoader type="custom" class="mb-10 h-6 w-72" />
+    <SkeletonLoader type="custom" class="h-40 w-full" />
+  </div>
+
+  <div v-else-if="story">
+    <div v-if="'teaser_image' in story.content">
+      <CmsImage :blok="story.content" is-teaser />
+    </div>
+    <div class="container">
+      <div class="py-4">
+        <Breadcrumbs
+          :items="[
+            { value: 'Home', to: '/' },
+            { value: story.name, to: story.slug },
+          ]">
+        </Breadcrumbs>
+      </div>
+      <Story :story="story" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+const slug = computed(() => route.params.slug)
+const { fetching, fetchBySlug, data: story } = useCms(`content-page-${slug}`)
+await fetchBySlug(`c/${slug.value as string}`)
+</script>
+
+<script lang="ts">
+export default {
+  name: 'ContentPage',
+  meta: {
+    pageType: 'content_pages',
+  },
+}
+</script>
