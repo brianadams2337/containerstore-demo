a950ae890dcdcc114822292a8ae5669164125c3f
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2023-09-22 14:09:16 +0200

    Rename icons to match other icon names

--- composables/useCareIcons.ts
+++ composables/useCareIcons.ts
@@ -1,38 +1,38 @@
 const symbolMap: Record<number, string> = {
-  720: 'machine_wash_30_gentle_or_delicate',
-  741: 'machine_wash_30_permanent_press',
-  739: 'machine_wash_30',
-  721: 'machine_wash_40_gentle_or_delicate',
-  723: 'machine_wash_40_permanent_press',
-  724: 'machine_wash_40',
-  722: 'machine_wash_60_permanent_press',
-  730: 'machine_wash_60',
+  720: 'machine-wash-30-gentle-or-delicate',
+  741: 'machine-wash-30-permanent-press',
+  739: 'machine-wash-30',
+  721: 'machine-wash-40-gentle-or-delicate',
+  723: 'machine-wash-40-permanent-press',
+  724: 'machine-wash-40',
+  722: 'machine-wash-60-permanent-press',
+  730: 'machine-wash-60',
   719: '', // 95 °C pflegeleichte Wäsche
-  728: 'machine_wash_95',
-  743: 'bleach_if_needed',
-  736: 'oxygen_bleach_if_needed',
+  728: 'machine-wash-95',
+  743: 'bleach-if-needed',
+  736: 'oxygen-bleach-if-needed',
   731: 'iron',
-  734: 'hand_wash',
-  746: 'iron_high',
-  725: 'dry_flat',
-  738: 'iron_medium',
+  734: 'hand-wash',
+  746: 'iron-high',
+  725: 'dry-flat',
+  738: 'iron-medium',
   // 744: '', // nicht auswringen
-  757: 'do_not_bleach',
-  750: 'do_not_iron',
-  727: 'do_not_dry_clean',
-  726: 'iron_low',
+  757: 'do-not-bleach',
+  750: 'do-not-iron',
+  727: 'do-not-dry-clean',
+  726: 'iron-low',
   // 729: '', //Nicht mit Dampf bügeln
-  762: 'do_not_tumble_dry',
-  733: 'do_not_wash',
+  762: 'do-not-tumble-dry',
+  733: 'do-not-wash',
   754: '', // Schonend reinigen mit Kohlenwasserstofflösungsmittel
   732: '', // Schonend reinigen mit Perchlorethylen
   758: '', // Trockenreinigung
   751: '', // Trockenreinigung mit Kohlenwasserstofflösungsmittel
   753: '', // Trockenreinigung mit Perchlorethylen
-  737: 'tumble_dry_at_low_temperature',
-  745: 'tumble_dry_at_normal_temperature',
-  768: 'tumble_dry',
-  777: 'drip_dry',
+  737: 'tumble-dry-at-low-temperature',
+  745: 'tumble-dry-at-normal-temperature',
+  768: 'tumble-dry',
+  777: 'drip-dry',
 }
 
 export function useCareIcons() {
