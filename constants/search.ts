09334cc2ac054a038254d96474800a5d7c10763c
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2023-10-02 17:30:03 +0200

    Add search with params type

--- constants/search.ts
+++ constants/search.ts
@@ -1,9 +1,11 @@
+import { SearchWith } from '@scayle/storefront-nuxt'
+
 export const DEBOUNCED_SEARCH_DURATION = 500 // in milliseconds
 export const MIN_CHARS_FOR_SEARCH = 3
 export const PRODUCT_LIMIT = 6
 export const CATEGORY_LIMIT = 5
 
-export const SEARCH_WITH_PARAMS = {
+export const SEARCH_WITH_PARAMS: SearchWith = {
   products: {
     attributes: {
       withKey: ['color', 'brand', 'name'],
