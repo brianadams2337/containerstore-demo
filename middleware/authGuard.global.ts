352cd98344fd50bd6440986aff11010f67d8c566
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2023-10-04 09:24:09 +0200

    Allow checkout route for the guest user

--- middleware/authGuard.global.ts
+++ middleware/authGuard.global.ts
@@ -3,25 +3,38 @@ export default defineNuxtRouteMiddleware(async (to) => {
 
   const localePath = useLocalePath()
 
-  const protectedRoutes = Object.values(
-    usePick(routeList, [
-      'account',
-      'orders',
-      'orderDetail',
-      'checkout',
-      'user',
-    ]),
-  ).map((it) => localePath(it.path))
+  const getProtectedRoutes = (exclude: (keyof LinkList)[] = []) => {
+    const routes = Object.entries(
+      usePick(routeList, [
+        'account',
+        'orders',
+        'orderDetail',
+        'checkout',
+        'user',
+      ]),
+    ).reduce((routeMap, [name, value]) => {
+      if (exclude.some((routeName) => routeName === name)) {
+        return routeMap
+      }
+
+      Object.assign(routeMap, { [name]: value })
+      return routeMap
+    }, {} as Partial<LinkList>)
+
+    return Object.values(routes).map((it) => localePath(it.path))
+  }
 
   const isGuest = !!user.value?.status?.isGuestCustomer
 
-  const isProtectedRoute = protectedRoutes.some((path) => to.path === path)
+  const isProtectedRoute = getProtectedRoutes().some((path) => to.path === path)
+  const isProtectRouteForGuest =
+    isGuest && getProtectedRoutes(['checkout']).some((path) => to.path === path)
 
   if (!isLoggedIn.value && isProtectedRoute) {
     return navigateTo({ path: localePath(routeList.signin.path) })
   }
 
-  if (isLoggedIn.value && isGuest && isProtectedRoute) {
+  if (isLoggedIn.value && isProtectRouteForGuest) {
     return navigateTo({ path: localePath(routeList.home.path) })
   }
 })
