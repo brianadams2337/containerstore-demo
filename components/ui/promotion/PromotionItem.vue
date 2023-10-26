a828d2853641f5eeed67a06cd7c6d053fd4bc46d
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2023-10-26 21:06:47 +0200

    Align start terms button content

--- components/ui/promotion/PromotionItem.vue
+++ components/ui/promotion/PromotionItem.vue
@@ -16,11 +16,11 @@
     <div class="mt-1 text-gray-500">
       <AppButton
         type="raw"
-        class="flex items-center justify-between whitespace-pre-line !pl-0 text-start text-xs font-semibold text-gray-500"
+        class="flex !items-start justify-between whitespace-pre-line !pl-0 text-start text-xs font-semibold text-gray-500"
         is-full-width
         size="xs"
         @click="toggleTerms">
-        <span class="inline-flex items-center">
+        <span class="inline-flex items-start">
           <IconInfoOutline class="mr-1 h-4 w-4" />
           {{ $t('promotion.terms') }}
         </span>
