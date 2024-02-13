a43dfa14450aeb47d2f1f5f5f45985fc33aecd8f
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2024-02-13 20:08:41 +0100

    fix(nuxt): Add completed PE progress message when promo is not applied

--- components/promotion/progress/PromotionFullProgressLabel.vue
+++ components/promotion/progress/PromotionFullProgressLabel.vue
@@ -1,20 +1,39 @@
 <template>
-  <p class="font-medium" :class="{ 'text-xs leading-5': isSmall }">
-    🎉
-    {{ $t('promotion.full_progress_message.first_part') }}
-    <span class="font-bold">
-      {{
-        $t('promotion.full_progress_message.middle_part', {
-          amount: formattedDiscount,
-        })
-      }}
-    </span>
-    {{ $t('promotion.full_progress_message.last_part') }}
+  <p
+    class="font-medium"
+    :class="{ 'text-xs leading-4': isSmall || !isMOVPromotionApplied }"
+  >
+    <template v-if="isMOVPromotionApplied">
+      🎉
+      {{ $t('promotion.full_progress_message.first_part') }}
+      <span class="font-bold">
+        {{
+          $t('promotion.full_progress_message.middle_part', {
+            amount: formattedDiscount,
+          })
+        }}
+      </span>
+      {{ $t('promotion.full_progress_message.last_part') }}
+    </template>
+    <template v-else>
+      {{ $t('promotion.cart_reached.first_part') }}
+      <span class="font-bold">
+        {{
+          $t('promotion.cart_reached.middle_part', {
+            discount: automaticDiscount,
+          })
+        }}
+      </span>
+      {{ $t('promotion.cart_reached.last_part') }}
+    </template>
   </p>
 </template>
 
 <script setup lang="ts">
 withDefaults(defineProps<{ isSmall?: boolean }>(), { isSmall: false })
 
-const { formattedDiscount } = await usePromotionProgress()
+const { formattedDiscount, isMOVPromotionApplied } =
+  await usePromotionProgress()
+
+const { automaticDiscount } = useCurrentPromotion()
 </script>
--- composables/usePromotionProgress.ts
+++ composables/usePromotionProgress.ts
@@ -4,13 +4,15 @@ export async function usePromotionProgress() {
   const { currentPromotion } = useCurrentPromotion()
   const { formatCurrency } = useFormatHelpers()
 
-  const minOrderValue = computed(() => {
+  const minOrderValue = computed<number>(() => {
     return currentPromotion.value?.customData?.minOrderValue || 0
   })
 
-  const minOrderAmount = computed(() => divideByHundred(minOrderValue.value))
+  const minOrderAmount = computed<number>(() => {
+    return divideByHundred(minOrderValue.value)
+  })
 
-  const basketTotal = computed(() => {
+  const basketTotal = computed<number>(() => {
     const promotionReductions = _sum(
       basketData.value.cost.appliedReductions
         .filter(({ category }) => category === 'promotion')
@@ -20,7 +22,7 @@ export async function usePromotionProgress() {
     return basketData.value.cost.withTax + promotionReductions
   })
 
-  const progress = computed(() => {
+  const progress = computed<number>(() => {
     if (!minOrderValue.value) {
       return 0
     }
@@ -28,18 +30,18 @@ export async function usePromotionProgress() {
     return basketTotal.value / minOrderAmount.value
   })
 
-  const isFullProgress = computed(() => {
+  const isFullProgress = computed<boolean>(() => {
     return !!progress.value && progress.value >= 100
   })
 
-  const formattedAmountLeft = computed(() => {
+  const formattedAmountLeft = computed<string | undefined>(() => {
     if (!minOrderValue.value) {
       return
     }
     return formatCurrency(minOrderValue.value - basketTotal.value)
   })
 
-  const formattedDiscount = computed(() => {
+  const formattedDiscount = computed<string | undefined>(() => {
     const promotedItem = basketData.value.items.find(
       (item) => item.promotionId === currentPromotion.value?.id,
     )
@@ -55,11 +57,21 @@ export async function usePromotionProgress() {
     return formatCurrency(reduction?.amount.absoluteWithTax)
   })
 
+  const isMOVPromotionApplied = computed<boolean>(() => {
+    if (!minOrderValue.value) {
+      return false
+    }
+    return basketData.value.items.some(({ promotionId }) => {
+      return promotionId === currentPromotion.value?.id
+    })
+  })
+
   return {
     minOrderAmount,
     progress,
     isFullProgress,
     formattedAmountLeft,
     formattedDiscount,
+    isMOVPromotionApplied,
   }
 }
