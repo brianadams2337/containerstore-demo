5895f6e4522acd6176c3ce2585daa9bde12fb29d
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2024-02-19 12:37:25 +0100

    fix(nuxt): Don't trigger content_view on server

--- middleware/routeChangeTrackingObserver.global.ts
+++ middleware/routeChangeTrackingObserver.global.ts
@@ -4,6 +4,10 @@ export default defineNuxtRouteMiddleware((to, from) => {
   const { pageState } = usePageState()
   const tracking = useTracking()
 
+  if (import.meta.server) {
+    return
+  }
+
   // We need to add delay because title is not properly retrieved from "document".
   setTimeout(() => {
     tracking.push(
