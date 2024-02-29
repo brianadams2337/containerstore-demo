2448a68641790bc45340bb965c6f4006b6f9d00d
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2024-02-29 16:28:16 +0100

    Extract store availability integration into separate component

---
+++ components/locator/StoreVariantAvailability.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="mt-4 flex items-center rounded border border-status-inactive p-5">
+    <div v-if="!!selectedStoreData">
+      <div class="flex items-start space-x-2">
+        <div
+          class="mt-2 size-2 rounded-full"
+          :class="{
+            'bg-green-neon': selectedStoreData.available,
+            'bg-red': !selectedStoreData.available,
+          }"
+        ></div>
+        <div class="flex flex-col">
+          <div class="font-bold">
+            {{
+              selectedStoreData.available
+                ? $t('store_locator.labels.available')
+                : $t('store_locator.labels.not_available')
+            }}
+          </div>
+          <div class="text-xs">
+            {{ selectedStoreData.storeName }}
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-else>
+      <p class="font-bold text-black">
+        {{ $t('store_locator.labels.store_availability') }}
+      </p>
+      <p class="text-xs text-primary">
+        {{ $t('store_locator.labels.store_availability_subline') }}
+      </p>
+    </div>
+    <AppButton
+      data-test-id="choose-store-button"
+      type="tertiary"
+      class="ml-auto rounded border border-black p-2 text-xs !normal-case"
+      rounded
+      @click="toggleStoreLocator"
+    >
+      {{
+        selectedStoreId
+          ? $t('store_locator.buttons.change_store')
+          : $t('store_locator.buttons.choose_store')
+      }}
+    </AppButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  selectedStoreId?: number
+  variantId?: number
+}
+
+interface StoreVariantInfo {
+  available: boolean
+  storeName: string
+}
+
+const { storeVariantData, refreshStoreVariant } = useStoreLocator()
+
+const { toggle: toggleStoreLocator } = useSlideIn('StoreLocatorSlideIn')
+
+const props = defineProps<Props>()
+
+const selectedStoreData = computed<StoreVariantInfo | undefined>(() => {
+  if (storeVariantData.value) {
+    return {
+      available: !!storeVariantData.value.items.length,
+      storeName: storeVariantData.value.name,
+    }
+  }
+})
+
+watch(
+  [() => props.selectedStoreId, () => props.variantId],
+  ([storeId, variantId]) => {
+    if (variantId && storeId) {
+      refreshStoreVariant(variantId, storeId)
+    }
+  },
+  { immediate: true },
+)
+</script>
--- pages/p/[slug].vue
+++ pages/p/[slug].vue
@@ -136,55 +136,11 @@
               @click:service-selection="onAddOnSelected"
             />
 
-            <div
+            <StoreVariantAvailability
               v-if="hasOneSizeVariantOnly || activeVariant"
-              class="mt-4 flex items-center rounded border border-status-inactive p-5"
-            >
-              <div v-if="!!selectedStoreData">
-                <div class="flex items-start space-x-2">
-                  <div
-                    class="mt-2 size-2 rounded-full"
-                    :class="{
-                      'bg-green-neon': selectedStoreData.available,
-                      'bg-red': !selectedStoreData.available,
-                    }"
-                  ></div>
-                  <div class="flex flex-col">
-                    <div class="font-bold">
-                      {{
-                        selectedStoreData.available
-                          ? $t('store_locator.labels.available')
-                          : $t('store_locator.labels.not_available')
-                      }}
-                    </div>
-                    <div class="text-xs">
-                      {{ selectedStoreData.storeName }}
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div v-else>
-                <p class="font-bold text-black">
-                  {{ $t('store_locator.labels.store_availability') }}
-                </p>
-                <p class="text-xs text-primary">
-                  {{ $t('store_locator.labels.store_availability_subline') }}
-                </p>
-              </div>
-              <AppButton
-                data-test-id="choose-store-button"
-                type="tertiary"
-                class="ml-auto rounded border border-black p-2 text-xs !normal-case"
-                rounded
-                @click="toggleStoreLocator"
-              >
-                {{
-                  selectedStoreId
-                    ? $t('store_locator.buttons.change_store')
-                    : $t('store_locator.buttons.choose_store')
-                }}
-              </AppButton>
-            </div>
+              :selected-store-id="selectedStoreId"
+              :variant-id="activeVariant?.id"
+            />
 
             <div class="mt-4 flex h-12">
               <AppButton
@@ -286,34 +242,7 @@ const selectedStoreId = ref<number | undefined>(
   favoriteStoreId.value ?? undefined,
 )
 
-const { toggle: toggleStoreLocator } = useSlideIn('StoreLocatorSlideIn')
 const { addItemToBasket, basketIdle } = await useProductDetailsBasketActions()
-
-const { storeVariantData, refreshStoreVariant } = useStoreLocator()
-
-interface StoreVariantInfo {
-  available: boolean
-  storeName: string
-}
-
-const selectedStoreData = computed<StoreVariantInfo | undefined>(() => {
-  if (storeVariantData.value) {
-    return {
-      available: !!storeVariantData.value.items.length,
-      storeName: storeVariantData.value.name,
-    }
-  }
-})
-
-watch(
-  [() => selectedStoreId.value, () => activeVariant.value?.id],
-  ([storeId, variantId]) => {
-    if (variantId && storeId) {
-      refreshStoreVariant(variantId, storeId)
-    }
-  },
-  { immediate: true },
-)
 
 const {
   isBuyXGetYPrioritized,
