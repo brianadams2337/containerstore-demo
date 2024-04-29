b73c409f01026017e872050196a2997bbefe586a
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2024-04-29 15:11:01 +0200

    fix: @opentelemetry/instrumentation missing in docker build

--- modules/opentelemetry/index.ts
+++ modules/opentelemetry/index.ts
@@ -14,7 +14,7 @@ import { register, createRequire } from 'node:module';
 import { pathToFileURL } from 'node:url'
 
 const require = createRequire(import.meta.url)
-register(pathToFileURL(require.resolve("@opentelemetry/instrumentation/hook.mjs")))
+register(pathToFileURL(require.resolve("./node-hooks.mjs")))
 
 // We should use dynamic imports after registering the customization hooks
 // https://nodejs.org/api/module.html#customization-hooks
@@ -83,6 +83,24 @@ export default defineNuxtModule<ModuleOptions>({
       if (newEntry) {
         nitro.options.virtual['#virtual/instrumented-entry'] = newEntry
         nitro.options.entry = '#virtual/instrumented-entry'
+      }
+
+      // For the node preset, we need to add module customization hooks
+      if (preset === 'node-server') {
+        nitro.hooks.hook('rollup:before', (_nitro, rollupConfig) => {
+          if (typeof rollupConfig.input === 'string') {
+            rollupConfig.input = [
+              rollupConfig.input,
+              resolver.resolve('src/node-hooks'),
+            ]
+          }
+          rollupConfig.output.entryFileNames = function (info) {
+            if (info.name === 'node-hooks') {
+              return 'node-hooks.mjs'
+            }
+            return 'index.mjs'
+          }
+        })
       }
     })
   },
