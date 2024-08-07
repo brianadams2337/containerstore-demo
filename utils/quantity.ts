d0c798025a0cfbbac2fc277b71cbcc9a00ff7675
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2024-08-07 12:44:36 +0200

    Fix calculation of quantity selection list

--- utils/quantity.ts
+++ utils/quantity.ts
@@ -1,9 +1,10 @@
 export const getQuantitySelectionList = (quantity = 0, excludeZero = false) => {
   const length = Math.max(Math.min(quantity, 9), 0)
 
-  const quantityList = Array.from({ length }, (_, index) => index)
+  const quantityListWithoutZero = Array.from(
+    { length },
+    (_, index) => index + 1,
+  )
 
-  const [_zero, ...quantityListWithoutZero] = quantityList
-
-  return excludeZero ? quantityListWithoutZero : quantityList
+  return excludeZero ? quantityListWithoutZero : [0, ...quantityListWithoutZero]
 }
