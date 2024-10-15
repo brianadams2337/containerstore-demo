fceb1290c0577599863da2a3e1df03856894b677
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2024-10-15 10:59:15 +0200

    refactor(templates/nuxt): fixed consistent usage of curly braces and typescript usage for playwright utils

--- playwright/support/lighthouseAudit.ts
+++ playwright/support/lighthouseAudit.ts
@@ -100,7 +100,11 @@ export const runLighthouseAudit = async (url: URL, auditedPage: string) => {
 }
 
 function calculateAverage(scores: number[]): number {
-  if (scores.length === 0) return 0
+  if (scores.length === 0) {
+    return 0
+  }
+
   const sum = scores.reduce((a, b) => a + b, 0)
+
   return sum / scores.length
 }
--- playwright/support/utils.ts
+++ playwright/support/utils.ts
@@ -9,14 +9,17 @@ export const isMobile = (page: Page) => {
 export async function getAllLinksFromPage(page: Page) {
   const link = page.getByRole('link')
   const allLinks = await link.all()
-  const allLinkHrefs = await Promise.all(
+  const allLinkHrefs: (string | null)[] = await Promise.all(
     allLinks.map((link) => link.getAttribute('href')),
   )
+
   return allLinkHrefs.reduce((links, link) => {
     expect(link, 'link has no a proper href').not.toBeFalsy()
 
-    if (link && !link?.startsWith('mailto:') && !link?.startsWith('#'))
+    if (link && !link?.startsWith('mailto:') && !link?.startsWith('#')) {
       links.add(new URL(link, page.url()).href)
+    }
+
     return links
   }, new Set<string>())
 }
