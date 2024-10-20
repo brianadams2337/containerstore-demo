26abdcf720229502bd11d108b7dfcabc8aee6161
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2024-10-20 20:33:33 +0200

    chore(nuxt): remove `useListingUiState.ts`

--- modules/cms/providers/contentful/components/Product.vue
+++ modules/cms/providers/contentful/components/Product.vue
@@ -1,25 +1,16 @@
 <template>
-  <div class="grid grid-cols-8">
-    <ProductCard
-      :key="`product-disruptor-${product.id}`"
-      :class="{
-        'col-span-full': width === '1' || width === undefined,
-        'col-span-full md:col-span-4 md:col-start-3':
-          (width === '2' && listingColumns === 3) || listingColumns === 1,
-        'col-span-full md:col-span-8': width === '2' && listingColumns === 2,
-      }"
-      :product="product"
-      :multiple-images="multipleImages"
-      class="col-span-full"
-      @intersect:product="emit('intersect:product', product)"
-    />
-  </div>
+  <ProductCard
+    :key="`product-disruptor-${product.id}`"
+    :product="product"
+    :multiple-images="multipleImages"
+    class="col-span-full"
+    @intersect:product="emit('intersect:product', product)"
+  />
 </template>
 
 <script setup lang="ts">
 import { defineOptions } from 'vue'
 import type { Product } from '@scayle/storefront-nuxt'
-import { useListingUiState } from '~/composables/useListingUiState'
 // TODO: This needs to be decoupled from the CMS module as it is coming from the SFB local components
 import ProductCard from '~/components/product/card/ProductCard.vue'
 
@@ -30,8 +21,6 @@ type CMSProductProps = {
 }
 
 defineProps<CMSProductProps>()
-
-const { listingColumns } = useListingUiState()
 
 const emit = defineEmits(['intersect:product'])
 
--- modules/cms/providers/storyblok/components/ProductSlider.vue
+++ modules/cms/providers/storyblok/components/ProductSlider.vue
@@ -21,7 +21,7 @@
       <CMSProduct
         v-for="(product, index) in products"
         :key="`product-slider-item-${product.id}`"
-        class="box-content w-1/2 shrink-0 snap-start snap-always px-px first:pl-5 last:pr-5 sm:w-1/5 sm:px-0.5 sm:first:pl-14 sm:last:pr-14"
+        class="box-content w-1/2 shrink-0 snap-start snap-always px-2 first:pl-5 last:pr-5 sm:w-1/5 sm:first:pl-14 sm:last:pr-14"
         :product="product"
         :fetching="fetching"
         @click:product="trackProductClick({ product: $event, index })"
