c4815d6cb1184a11eb5fc68430dbe552e016a56b
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2025-04-04 13:43:43 +0200

    fix(nuxt): Adjust product card skeleton loader to reflect real layout

---
+++ .changeset/calm-owls-ring.md
@@ -0,0 +1,5 @@
+---
+'@scayle/storefront-application-nuxt': minor
+---
+
+Adjust `/components/product/card/SFProductCardSkeleton.vue` to better reflect the real layout of `/components/product/card/SFProductCard.vue`.
--- components/product/card/SFProductCardSkeleton.vue
+++ components/product/card/SFProductCardSkeleton.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="col-span-6 w-full md:col-span-4 lg:col-span-3 lg:max-w-[23rem]">
+  <div class="col-span-6 mb-5 w-full lg:col-span-4 xl:col-span-3">
     <SFSkeletonLoader
       type="custom"
-      class="mb-5 min-h-60 rounded-lg bg-gray-200 md:min-h-56 lg:min-h-80"
+      class="mb-2 aspect-3/4 rounded-lg bg-gray-200 md:mb-3"
     />
+
+    <SFSkeletonLoader type="custom" class="mb-3 size-12 rounded-md md:hidden" />
+
     <SFSkeletonLoader
       type="custom"
-      class="mb-1 h-3 max-w-[6.5rem] rounded-lg bg-gray-200"
+      class="mb-3 h-3 max-w-[6.5rem] rounded-lg bg-gray-200"
     />
     <SFSkeletonLoader
       type="custom"
