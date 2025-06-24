d6da45877d7415ff521c17112d4bcf875df381aa
${GIT_PUBLIC_USER_NAME} <${GIT_PUBLIC_USER_EMAIL}>
2025-06-24 16:16:13 +0200

    fix(nuxt): Truncate SFShowAllResultsLink

---
+++ .changeset/light-boxes-leave.md
@@ -0,0 +1,5 @@
+---
+'@scayle/storefront-application-nuxt': patch
+---
+
+**\[Search\]** Updated `SFShowAllResultsLink.vue` to fit into smaller containers by truncating its label. This prevents text overflow and maintains consistent UI layout.
--- components/search/SFShowAllResultsLink.vue
+++ components/search/SFShowAllResultsLink.vue
@@ -4,10 +4,10 @@
     data-testid="display-all-results"
     :to="getSearchRoute(searchTerm)"
     :aria-label="showMoreAriaLabel"
-    class="h-11 rounded transition-all duration-150 hover:right-1 hover:bg-gray-200 hover:px-1.5 focus:bg-gray-200 focus:px-1.5 lg:h-5"
+    class="!flex h-11 max-w-fit rounded transition-all duration-150 hover:right-1 hover:bg-gray-200 hover:px-1.5 focus:bg-gray-200 focus:px-1.5 lg:h-5"
   >
     <span
-      class="text-md font-normal leading-5 text-secondary max-lg:py-3 lg:text-md"
+      class="truncate text-md font-normal leading-5 text-secondary max-lg:py-3 lg:text-md"
       v-html="showMoreLabel"
     />
   </SFLocalizedLink>
